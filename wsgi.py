360a955f0f1d8aabe1e52863fbe2054ce1510749
T-Tykesson <75175408+T-Tykesson@users.noreply.github.com>
2023-12-05 16:42:17 +0100

    modified:   0_start_here.ipynb 	modified:   1_run_flask.ipynb 	modified:   requirements.txt 	new file:   templates/base.html 	new file:   templates/index.html 	new file:   templates/login.html 	new file:   venv/Scripts/Activate.ps1 	new file:   venv/Scripts/activate 	new file:   venv/Scripts/activate.bat 	new file:   venv/Scripts/deactivate.bat 	new file:   venv/Scripts/flask.exe 	new file:   venv/Scripts/gunicorn.exe 	new file:   venv/Scripts/ipython.exe 	new file:   venv/Scripts/ipython3.exe 	new file:   venv/Scripts/jupyter-kernel.exe 	new file:   venv/Scripts/jupyter-kernelspec.exe 	new file:   venv/Scripts/jupyter-migrate.exe 	new file:   venv/Scripts/jupyter-run.exe 	new file:   venv/Scripts/jupyter-troubleshoot.exe 	new file:   venv/Scripts/jupyter.exe 	new file:   venv/Scripts/pip.exe 	new file:   venv/Scripts/pip3.10.exe 	new file:   venv/Scripts/pip3.11.exe 	new file:   venv/Scripts/pip3.exe 	new file:   venv/Scripts/pygmentize.exe 	new file:   venv/Scripts/python.exe 	new file:   venv/Scripts/pythonw.exe 	new file:   venv/Scripts/pywin32_postinstall.py 	new file:   venv/Scripts/pywin32_testall.py 	new file:   venv/pyvenv.cfg 	new file:   venv/share/jupyter/kernels/python3/kernel.json 	new file:   venv/share/jupyter/kernels/python3/logo-32x32.png 	new file:   venv/share/jupyter/kernels/python3/logo-64x64.png 	new file:   venv/share/jupyter/kernels/python3/logo-svg.svg 	new file:   venv/share/man/man1/ipython.1 	modified:   wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,69 @@
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, redirect, url_for, request
 from prediction import predict
+import webbrowser
+import threading
+import os
+import queue
+import datetime
+import backend_handeler
+from backend_handeler import UserInputData
+from back_end.api_counter import (InternalServerError,
+                                  NetatmoGeneralError, NoActiveTokenError,
+                                  NoApiCallsLeftError, InvalidInputError)
 
-application = Flask(__name__)
+from babel.numbers import *  # Included due to hidden imports tkcalendar
+from babel.dates import format_date, parse_date, get_day_names, get_month_names
+import calendar
+import openpyxl
 
 
-@application.route('/')
-@application.route('/status')
-def status():
-    return jsonify({'status': 'ok'})
+app = Flask(__name__)
 
 
-@application.route('/predictions', methods=['POST'])
+@app.route('/')
+def home():
+    #return "Hello, Flask!"
+    return redirect(url_for("login"))
+
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        auth_key = request.form["auth"]
+        start_date = request.form.get("start_date")
+        end_date = request.form.get("end_date")
+        print(auth_key, "\n", start_date, "\n"
+              , end_date)
+        
+        input_data = UserInputData(
+                auth_token,
+                latitude,
+                longitude,
+                date_begin,
+                date_end,
+                scale,
+                amount,
+                directory
+            )
+
+        backend_handeler.run_program(input_data)
+
+        return redirect(url_for("user", usr=auth_key))
+    else: 
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+
+@app.route('/predictions', methods=['POST'])
 def create_prediction():
     data = request.data or '{}'
     body = json.loads(data)
     return jsonify(predict(body))
+
+print(app.url_map)
+
+if __name__=="__main__":
+    app.run(debug=True)
