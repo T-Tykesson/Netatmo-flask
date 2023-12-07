d85c0b466fc4a76fd571c6a7b7f6b294984b5464
T-Tykesson <75175408+T-Tykesson@users.noreply.github.com>
2023-12-07 12:21:43 +0100

    Update data_processing.py

--- back_end/data_processing.py
+++ back_end/data_processing.py
@@ -18,24 +18,24 @@ from back_end.api_counter import (InternalServerError, NoDataInStationError,
 
 def quickselect_median(lst):
     """
-
+    Quickselect implementation to find median.
 
     Parameters
     ----------
-    lst : TYPE
-        DESCRIPTION.
+    lst : list
+        The input lists from which median should be found.
 
     Raises
     ------
     ValueError
-        DESCRIPTION.
+        If empty list or mismatched lengths, raises ValueError.
 
     Returns
     -------
-    TYPE
-        DESCRIPTION.
-    TYPE
-        DESCRIPTION.
+    name : str
+        name of median station.
+    median_value : float
+        the median value.
 
     """
     if len(lst[0]) != len(lst[1]):
@@ -58,19 +58,19 @@ def quickselect_median(lst):
 
 def quickselect(lst, k):
     """
-
+    Do recuesive quickselect on list with name and value.
 
     Parameters
     ----------
-    lst : TYPE
-        DESCRIPTION.
-    k : TYPE
-        DESCRIPTION.
+    lst : list
+        2 dimensional list with name on dimension 0 and values on dimension 1.
+    k : int
+        Helper variable for quickselect.
 
     Returns
     -------
-    TYPE
-        DESCRIPTION.
+    pivots : list
+        list of [name, value]
 
     """
     if len(lst[1]) == 1:
@@ -98,24 +98,30 @@ def quickselect(lst, k):
 
 def convert_to_unix_from_stations(station_data_list):
     """
-
+    Get all unix values from stations and put them in a numpy array.
 
     Parameters
     ----------
-    station_data_list : TYPE
-        DESCRIPTION.
+    station_data_list : list
+        List of numpy arrays with with dates on dimension 0, data on dimension 1 and
+        Unix dates on dimension 2 for input station.
+
+    Raises
+    ------
+    IndexError
+        Error if problem with the shape of station_data_array.
 
     Returns
     -------
-    unix_from_stations : TYPE
-        DESCRIPTION.
+    unix_from_stations : numpy array
+        Numpy array containting UNIX date values from input list
 
     """
     unix_from_stations = []
     try:
         for station_data in station_data_list:
             station_data_array = np.array(station_data)
-            if station_data != []:
+            if np.any(station_data):
                 unix_from_station = station_data_array[2, :]
                 unix_from_station = unix_from_station.astype(int)
                 unix_from_stations.append(unix_from_station)
@@ -128,27 +134,37 @@ def convert_to_unix_from_stations(station_data_list):
     unix_from_stations = np.array(unix_from_stations, dtype=object)
     return unix_from_stations
 
-
 def find_what_data_each_time_step(station_data_list, station_list,
                                   unix_from_stations, time_step_list):
     """
+    Construct a dictionary with rainstations for each time step with data.
 
+    The function creates a dictionary with time steps as keys and the stations
+    that have data for that time step as values. If no value exists for the
+    time step it is left blank.
+
+    The function checks 15 minutes to the left and right of the time step value
+    in order to account for offset data and adds it to the dictonary.
+    TODO: Should it always be 15 minutes here?
 
     Parameters
     ----------
-    station_data_list : TYPE
-        DESCRIPTION.
-    station_list : TYPE
-        DESCRIPTION.
-    unix_from_stations : TYPE
-        DESCRIPTION.
-    time_step_list : TYPE
-        DESCRIPTION.
+    station_data_list : numpy array
+        A numpy array with dates on dimension 0, data on dimension 1 and
+        Unix dates on dimension 2 for input station.
+    station_list : list
+        List of RainStation objects.
+    unix_from_stations : numpy array
+        Numpy array containting UNIX date values from input list
+    time_step_list : list
+        List of unix dates with time steps that is used to check if there
+        is data near those values.
 
     Returns
     -------
-    data_dict : TYPE
-        DESCRIPTION.
+    data_dict : dict
+        A dictionary with timesteps as keys and corresponding stations as
+        values.
 
     """
     data_dict = {}
@@ -162,9 +178,8 @@ def find_what_data_each_time_step(station_data_list, station_list,
                     # 15 min both ways
                     if time_step in data_dict:
                         if np.abs(time_step - int(station_data_list[i][2, j])) != 0:
-                            print("inte noll utan:",
+                            print("Nu blev det fel, 0 förväntades men fick:",
                                   (time_step - int(station_data_list[i][2, j])))
-
                         data_dict[time_step].append(
                             [station_list[i],
                              float(station_data_list[i][1, j])])
@@ -174,22 +189,32 @@ def find_what_data_each_time_step(station_data_list, station_list,
                               float(station_data_list[i][1, j])]]
             except IndexError:
                 counter += 1
-                #print("station_list_i", station_data_list[i])
-                #print("station_list", station_data_list)
                 continue
             except ValueError:
                 counter2 += 1
-                # print(unix)
-                # print(time_step)
                 continue
 
-    print("counter 1", counter)
-    print("counter 2", counter)
+    # print("counter 1", counter)
+    # print("counter 2", counter2)
     return data_dict
 
-
 def format_median_data_view(data_dict, reference_coordinate):
+"""
+    Calculate median value for each time step and create a pandas data frame.
 
+    Parameters
+    ----------
+    data_dict : dict
+        A dictionary with timesteps as keys and corresponding stations as
+        values.
+    reference_coordinate : str
+        String that specifies the reference coordinate, used in header as distance from.
+
+    Returns
+    -------
+    median_view_df : pandas dataframe
+        pandas data frame with median values for each time step.
+    """
     median_array = []
     for time_step, values in tqdm(data_dict.items()):
         if values:
@@ -205,12 +230,12 @@ def format_median_data_view(data_dict, reference_coordinate):
             if isinstance(time_step_station, tuple):
                 name = [station.get_name()
                         for station in time_step_station]
-                distance = [int(1000*round(station.get_distance(), 3))
+                distance = [int(1000 * round(station.get_distance(), 3))
                             for station in time_step_station]
             else:
                 name = time_step_station.get_name()
                 distance = int(
-                    1000*round(time_step_station.get_distance(), 3))
+                    1000 * round(time_step_station.get_distance(), 3))
 
             time_step = datetime.utcfromtimestamp(
                 int(time_step)).strftime('%Y-%m-%d %H:%M:%S')
@@ -220,33 +245,35 @@ def format_median_data_view(data_dict, reference_coordinate):
     median_array = np.array(median_array, dtype=object)
 
     try:
-        median_data_frame = pd.DataFrame(median_array, columns=[
+        median_view_df = pd.DataFrame(median_array, columns=[
             "Datum",
             "Medianvärde regn [mm]",
             "Stationsnamn",
             f"Avstånd från punkt {reference_coordinate} [m]"])
-    except ValueError as exc:
-        print(median_array)
-        raise ValueError(exc) from exc
 
-    return median_data_frame
+    except ValueError as exc:
+        raise ValueError("Ingen data kunde hittas vid skapande av datavy. \n"
+                         "Detta kan bland annat hända om vald period är"
+                         " kortar än valt tidssteg.") from exc
+
+    return median_view_df
 
 
 def format_standard_data_view(data_dict):
     """
-    Format the data in a certain way for excel 3d map function.
+    Format the data with station names as headers and time steps as rows.
 
     Parameters
     ----------
-    data_dict : TYPE
-        DESCRIPTION.
+    data_dict : dict
+        A dictionary with timesteps as keys and corresponding stations as
+        values.
 
     Returns
     -------
-    pandas_array_sheet_3 : TYPE
-        DESCRIPTION.
+    standard_view_df : pandas dataframe
+        pandas data frame with station names as headers and time steps as rows.
     """
-
     station_names = list(set(station.get_name() for data in data_dict.values()
                          for station, _ in data))
     data_rows = {station_name: [] for station_name in station_names}
@@ -274,20 +301,21 @@ def format_standard_data_view(data_dict):
     return standard_view_df
 
 
-def format_data_for_map_view(input_data, data_dict):
+
+def format_data_map_view(input_data, data_dict):
     """
-    Format the data in a certain way for excel 3d map function.
+    Format the data with each new row being a new data entry.
 
     Parameters
     ----------
-    data_dict : TYPE
-        DESCRIPTION.
+    data_dict : dict
+        A dictionary with timesteps as keys and corresponding stations as
+        values.
 
     Returns
     -------
-    pandas_array_sheet_3 : TYPE
-        DESCRIPTION.
-
+    map_view_df : pandas dataframe
+        pandas data frame with rows being new data entries.
     """
     data_rows = []
     for i, (time_step, values) in tqdm(enumerate(data_dict.items())):
@@ -317,12 +345,37 @@ def format_data_for_map_view(input_data, data_dict):
                 }
                 data_rows.append(row)
 
-    pandas_array_sheet_3 = pd.DataFrame(data_rows)
+    map_view_df = pd.DataFrame(data_rows)
 
-    return pandas_array_sheet_3
-
+    return map_view_df
+  
 
 def collect_station_data(input_data, rain_station_list, start_stop_list, gui=None):
+    """
+    For each station in station list, collect station data and return it.
+
+    Parameters
+    ----------
+    input_data : UserInputData object
+        An object of class UserInputData containing the users input data.
+    rain_station_list : list
+        List of RainStation objects.
+    start_stop_list : list
+        List of 2x1 matricies of start, stop value pairs for partitioning data.
+    gui : gui object, optional
+        A gui object to update gui elements. The default is None.
+
+    Raises
+    ------
+    NoApiCallsLeftError
+        If amount of api calls is exceeded raise error.
+
+    Returns
+    -------
+    rain_data_list : list
+        A list of data from stations in rain_station_list.
+
+    """
     rain_data_list = []
     for i, station in enumerate(rain_station_list):
         if gui is not None:
@@ -332,7 +385,7 @@ def collect_station_data(input_data, rain_station_list, start_stop_list, gui=Non
                 100 / (len(rain_station_list) + 1))))
 
         try:
-            station_data = rain_data.get_measure(
+            station_data = get_measure(
                 input_data,
                 station,
                 start_stop_list,
@@ -342,20 +395,47 @@ def collect_station_data(input_data, rain_station_list, start_stop_list, gui=Non
 
             rain_data_list.append(station_data)
 
-        except NoApiCallsLeftError:
+        except NoApiCallsLeftError as exc:
             if i > 0:
                 if gui is not None:
                     gui.event_queue.put((
-                        "message", "För många förfrågningar till Netatmo, sparar fil med redan hämtad data..."))
+                        "message", "För många förfrågningar till Netatmo,"
+                        "sparar fil med redan hämtad data..."))
                 return rain_data_list
 
-            raise NoApiCallsLeftError
+            raise NoApiCallsLeftError from exc
 
     return rain_data_list
 
 
 def create_data_views_for_excel(input_data, rain_station_list, start_stop_list,
                                 reference_coordinate, gui=None):
+    """
+    Create three separate data views of the data using pandas dataframes.
+
+    Parameters
+    ----------
+    input_data : UserInputData object
+        An object of class UserInputData containing the users input data.
+    rain_station_list : list
+        List of RainStation objects.
+    start_stop_list : List
+        List of 2x1 matricies of start, stop value pairs for partitioning data.
+    reference_coordinate : str
+        String that specifies the reference coordinate, used in header.
+    gui : gui object, optional
+        A gui object to update gui elements. The default is None.
+
+    Returns
+    -------
+    standard_view_df : pandas dataframe
+        Standard data view with names as headers and timesteps as rows.
+    median_df : pandas dataframe
+        Data frame that shows median values of stations for each timestep.
+    map_view_df : pandas dataframe
+        Data frame with format for easy manipulation with map function in excel.
+
+    """
 
     rain_data_list = collect_station_data(
         input_data,
@@ -363,16 +443,16 @@ def create_data_views_for_excel(input_data, rain_station_list, start_stop_list,
         start_stop_list,
         gui=gui
     )
-    # (rain_data_list)
+  
     time_step_list = np.arange(
         start_stop_list[0][0], start_stop_list[-1][1], 900, dtype=int
     )
 
     rain_station_list = rain_station_list[0:len(rain_data_list)]
-    # print(rain_station_list)
+
     indices_to_delete = []
     for i, value in enumerate(rain_data_list):
-        if value == []:
+        if not np.any(value):
             indices_to_delete.append(i)
 
     for index in reversed(indices_to_delete):
